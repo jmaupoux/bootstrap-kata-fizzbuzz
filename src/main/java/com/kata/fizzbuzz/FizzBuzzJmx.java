0ecbd17894fbbcb3888f43f56fecc5e6dbdfdf4a
Julien Maupoux <julien.maupoux2@ext.soprasteria.com>
2020-05-07 17:18:41 +0200

    fix tests

--- src/main/java/com/kata/fizzbuzz/FizzBuzzJmx.java
+++ src/main/java/com/kata/fizzbuzz/FizzBuzzJmx.java
@@ -3,6 +3,16 @@ package com.kata.fizzbuzz;
 public class FizzBuzzJmx
 {
     public String fizzbuzz(int i) {
-        return "";
+        if(i%5 == 0 && i%3 == 0){
+            return "FizzBuzz";
+        }
+        if(i%3==0){
+            return "Fizz";
+        }
+        if(i%5==0){
+            return "Buzz";
+        }
+
+        return i+"";
     }
 }
