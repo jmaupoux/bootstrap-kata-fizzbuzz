83b8546d62f71fc21e03de7f954318b5c1beedcb
Julien Maupoux <julien.maupoux2@ext.soprasteria.com>
2020-05-07 17:06:49 +0200

    Tests basiques

--- src/test/java/com/kata/fizzbuzz/FizzBuzzJmxTest.java
+++ src/test/java/com/kata/fizzbuzz/FizzBuzzJmxTest.java
@@ -14,4 +14,22 @@ class FizzBuzzJmxTest
     {
         assertEquals("1", fizzBuzz.fizzbuzz(1));
     }
+
+    @Test
+    void fizzbuzzFizz()
+    {
+        assertEquals("Fizz", fizzBuzz.fizzbuzz(3));
+    }
+
+    @Test
+    void fizzbuzzBuzz()
+    {
+        assertEquals("Buzz", fizzBuzz.fizzbuzz(5));
+    }
+
+    @Test
+    void fizzbuzz()
+    {
+        assertEquals("FizzBuzz", fizzBuzz.fizzbuzz(15));
+    }
 }
