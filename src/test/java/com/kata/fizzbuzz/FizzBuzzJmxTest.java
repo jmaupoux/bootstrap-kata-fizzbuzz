0bea687efd3bfa1e5971cfcc1296aa45f4bfbbda
Julien Maupoux <julien.maupoux2@ext.soprasteria.com>
2020-05-07 17:14:01 +0200

    enrich

--- src/test/java/com/kata/fizzbuzz/FizzBuzzJmxTest.java
+++ src/test/java/com/kata/fizzbuzz/FizzBuzzJmxTest.java
@@ -13,23 +13,33 @@ class FizzBuzzJmxTest
     void fizzbuzzOne()
     {
         assertEquals("1", fizzBuzz.fizzbuzz(1));
+        assertEquals("2", fizzBuzz.fizzbuzz(2));
+        assertEquals("4", fizzBuzz.fizzbuzz(4));
     }
 
     @Test
     void fizzbuzzFizz()
     {
         assertEquals("Fizz", fizzBuzz.fizzbuzz(3));
+        assertEquals("Fizz", fizzBuzz.fizzbuzz(9));
+        assertEquals("Fizz", fizzBuzz.fizzbuzz(18));
     }
 
     @Test
     void fizzbuzzBuzz()
     {
         assertEquals("Buzz", fizzBuzz.fizzbuzz(5));
+        assertEquals("Buzz", fizzBuzz.fizzbuzz(10));
+        assertEquals("Buzz", fizzBuzz.fizzbuzz(20));
+
     }
 
     @Test
     void fizzbuzz()
     {
         assertEquals("FizzBuzz", fizzBuzz.fizzbuzz(15));
+        assertEquals("FizzBuzz", fizzBuzz.fizzbuzz(30));
+        assertEquals("FizzBuzz", fizzBuzz.fizzbuzz(45));
     }
+
 }
